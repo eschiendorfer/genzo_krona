5d2ef757e1b94d28a1fc748530fbf4b447ff455a
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2018-03-08 01:07:21 +0100

    Add: Highlighting Coupons on Levels

--- classes/PlayerLevel.php
+++ classes/PlayerLevel.php
@@ -247,6 +247,7 @@ class PlayerLevel extends \ObjectModel {
 
             $coupon->code = ($prefix) ? $prefix.'-'.$code : $code;
             $coupon->active = true;
+            $coupon->highlight = 1;
             $coupon->add();
 
             \CartRule::copyConditions($id_cart_rule, $coupon->id);
