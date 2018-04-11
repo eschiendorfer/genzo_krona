afcfb051cf7999be9362512647e0a828dc535237
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2018-04-11 17:43:44 +0200

    Change: version number

--- genzo_krona.php
+++ genzo_krona.php
@@ -33,7 +33,7 @@ class Genzo_Krona extends Module
 	function __construct() {
 		$this->name = 'genzo_krona';
 		$this->tab = 'front_office_features';
-		$this->version = '1.0.3';
+		$this->version = '1.0.4';
 		$this->author = 'Emanuel Schiendorfer';
 		$this->need_instance = 0;
 
