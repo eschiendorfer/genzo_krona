8b0ac63ffe919c7a1cc10d447bc2cd486cc95809
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2023-12-08 18:55:19 +0100

    Using vanilla js instead of jquery

--- views/js/notification.js
+++ views/js/notification.js
@@ -1,4 +1,4 @@
-$(window).on('load', function() {
+document.addEventListener("DOMContentLoaded", function () {
     getNotification();
 });
 
@@ -8,19 +8,33 @@ setInterval(function() {
 
 function getNotification() {
     // id_customer is defined in HookDisplayHeader
-    $.ajax({
-        type: 'POST',
-        url: '/modules/genzo_krona/ajax.php',
-        data: {notification: id_customer},
-        datatype: 'json',
-        success: function (notification) {
-            notification = $.parseJSON(notification);
-            if (notification > 0) {
-                $('#krona-notification').show().text(notification)
-            }
-            else {
-                $('#krona-notification').hide();
+    var xhr = new XMLHttpRequest();
+    var url = '/modules/genzo_krona/ajax.php';
+    var params = 'notification=' + id_customer;
+    xhr.open('POST', url, true);
+
+    // Send the proper header information along with the request
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    // Set up a function to be called when the response is received
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            // Parse the JSON response
+            var notification = JSON.parse(xhr.responseText);
+            var container = document.getElementById('krona-notification');
+
+            // Check the notification value
+            if (container) {
+                if (notification > 0) {
+                    container.style.display = 'block';
+                    container.innerText = notification;
+                } else {
+                    container.style.display = 'none';
+                }
             }
         }
-    });
+    };
+
+    // Send the request
+    xhr.send(params);
 }
