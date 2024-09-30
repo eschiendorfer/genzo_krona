7330364d2f730f47b8e840111dc920dfbd2399d9
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2024-09-30 17:00:36 +0200

    Activate rank again to prevent php warning

--- genzo_krona.php
+++ genzo_krona.php
@@ -653,7 +653,7 @@ class Genzo_Krona extends Module
                 'pseudonym' => $player->display_name,
                 'avatar' => $player->avatar_full && file_exists(_PS_UPLOAD_DIR_.'genzo_krona/img/avatar/'.$player->avatar) ? $player->avatar_full : '/upload/genzo_krona/img/avatar/no-avatar.jpg',
                 'total' => $player->total . ' ' . $name,
-                // 'rank' => $player->getRank().' '.$this->l('from').' '.Player::getTotalPlayers(), Todo: bring back once $player->getRank was improved
+                'rank' => $player->getRank().' '.$this->l('from').' '.Player::getTotalPlayers(),
                 'level' => PlayerLevel::getLastPlayerLevel($id_customer)->name,
                 'url' => $this->context->link->getModuleLink('genzo_krona', 'overview').'/'.strtolower($player->referral_code),
             );
@@ -705,8 +705,9 @@ class Genzo_Krona extends Module
     public function hookDisplayHeader() {
 
         // CSS
-        $this->context->controller->addCSS($this->_path.'/views/css/krona.css');
-        $this->context->controller->addCSS($this->_path.'/views/css/krona_custom.css');
+        // Todo: reimplement this for public version (make setting to disable loading of css files)
+        // $this->context->controller->addCSS($this->_path.'/views/css/krona.css');
+        // $this->context->controller->addCSS($this->_path.'/views/css/krona_custom.css');
 
         // JS
 
